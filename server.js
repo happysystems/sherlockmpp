65c15ea5fb6630ef4b18dc0ebe63fa81b33f0e18
happysystems <lolhahaxdimhappy@outlook.com>
2025-02-08 22:20:06 -0500

    owo v1.0

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-// server.js
-
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = process.env.PORT || 3000; // You can change the port if needed
-
-// Serve the files from the ./mpp folder
-app.use("/mpp", express.static(path.join(__dirname, "mpp")));
-
-// Optional: You can serve the index file (e.g., index.html) as the default route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "mpp", "index.html"));
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+if (config.uws) {
+module.exports = require('uwebsockets').App({});
+} else module.exports = require('http').createServer(app);
